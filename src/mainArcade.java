32fcb66d30c09ec02c48ed1c4049600a9005108d
Mario Mendez <mendezmario2018@gmail.com>
2025-04-18 15:14:01 -0500

    added scramble game and fixed input

--- src/mainArcade.java
+++ src/mainArcade.java
@@ -1,22 +1,27 @@
 import java.util.Scanner;
+import Classes.*;
 
 public class mainArcade {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int choice ;
+    boolean keepPlaying = true;
 
+    while (keepPlaying){
     System.out.println("Welcome to the Arcade!");
-    System.out.println("Enters from 1 to 6 to play the game");
+    System.out.println("Enter numbers from 1 to 6 to play the game");
 
     System.out.println("1. Guess the Number");
     System.out.println("2. Minefield");
     System.out.println("3. Hangman");
     System.out.println("4. Rock Paper Scissors");
-    System.out.println("5. ??");
+    System.out.println("5. Word Scramble");
     System.out.println("6. ??");
     System.out.print("Enter your choice: ");
 
     choice = scanner.nextInt();
+    scanner.nextLine();
+
 
     switch (choice) {
         case 1:
@@ -35,7 +40,7 @@ public class mainArcade {
             break;
 
         case 5:
-
+            Classes.WordScramble.main(scanner);
             break;
 
         case 6:
@@ -47,6 +52,17 @@ public class mainArcade {
             break;
     
     }
-    scanner.close();
+    if (keepPlaying)  {
+        System.out.println("\nDo you want to return to arcade ?\n(1 for yes / 2 for no) ");
+        int again = scanner.nextInt();
+        scanner.nextLine();
+        if (again == 2){
+            keepPlaying = false;
+            System.out.println("Thanks for playing!");
+
+        }
+    }
+}
+    
     }
 }
