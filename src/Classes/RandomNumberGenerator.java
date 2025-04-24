04ab4a80520f3a064cbc09e99550c68363cac3db
Lorenzo Vee <134215240+AmazedAce@users.noreply.github.com>
2025-04-24 14:54:51 -0500

    Update RandomNumberGenerator.java

--- src/Classes/RandomNumberGenerator.java
+++ src/Classes/RandomNumberGenerator.java
@@ -1,73 +1,91 @@
-package Classes;
-import javax.swing.*;
-import java.awt.event.*;
-import java.util.*;
+package random.number.generator;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class RandomNumberGenerator {
 
     public static void main(String[] args) {
+        showMainMenu();
+    }
+
+    public static void showMainMenu() {
+        JFrame menuFrame = new JFrame("Main Menu");
+        menuFrame.setSize(300, 150);
+        menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menuFrame.setLayout(new GridLayout(3, 1));
+
+        JLabel welcomeLabel = new JLabel("Welcome to the Number Guessing Game!", SwingConstants.CENTER);
+        JButton startButton = new JButton("Start Game");
+        JButton exitButton = new JButton("Exit");
+
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                menuFrame.dispose(); // close the menu
+                startGame(); // launch game
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        menuFrame.add(welcomeLabel);
+        menuFrame.add(startButton);
+        menuFrame.add(exitButton);
+
+        menuFrame.setVisible(true);
+    }
+
+    public static void startGame() {
         JFrame frame = new JFrame("Guess the Number!");
-        frame.setSize(300, 200);
+        frame.setSize(300, 220);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel label = new JLabel("Enter a number from 1 to 10", SwingConstants.CENTER);
         JTextField textField = new JTextField();
         JButton guessButton = new JButton("Guess");
         JButton tryAgainButton = new JButton("Try Again");
+        JButton endGameButton = new JButton("End Game");
 
-        // Start with a new game
         NumberGame[] game = { new NumberGame() };
 
-        // GUESS BUTTON ACTION
-        guessButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    int guess = Integer.parseInt(textField.getText());
-                    if (guess == game[0].getNumber()) {
-                        label.setText("Correct! 🎉");
-                    } else {
-                        label.setText("Wrong! It was " + game[0].getNumber());
-                    }
-                    guessButton.setEnabled(false);
-                } catch (NumberFormatException ex) {
-                    label.setText("Please enter a number!");
+        guessButton.addActionListener(e -> {
+            try {
+                int guess = Integer.parseInt(textField.getText());
+                if (guess == game[0].getNumber()) {
+                    label.setText("Correct! 🎉");
+                } else {
+                    label.setText("Wrong! It was " + game[0].getNumber());
                 }
+                guessButton.setEnabled(false);
+            } catch (NumberFormatException ex) {
+                label.setText("Please enter a number!");
             }
         });
 
-        // TRY AGAIN BUTTON ACTION
-        tryAgainButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                game[0] = new NumberGame(); // reset the game with a new number
-                label.setText("Enter a number from 1 to 10!");
-                textField.setText("");
-                guessButton.setEnabled(true);
-            }
+        tryAgainButton.addActionListener(e -> {
+            game[0] = new NumberGame();
+            label.setText("Enter a number from 1 to 10");
+            textField.setText("");
+            guessButton.setEnabled(true);
         });
 
-        frame.setLayout(new java.awt.GridLayout(4, 1));
+        endGameButton.addActionListener(e -> {
+            frame.dispose(); // close the game window
+            showMainMenu(); // return to menu
+        });
+
+        frame.setLayout(new GridLayout(5, 1));
         frame.add(label);
         frame.add(textField);
         frame.add(guessButton);
         frame.add(tryAgainButton);
+        frame.add(endGameButton);
 
         frame.setVisible(true);
-    }
-    
-    
-}
-
-// The NumberGame class has been moved to its own file.
-class NumberGame {
-    private int number;
-
-    public NumberGame() {
-        Random random = new Random();
-        this.number = random.nextInt(10) + 1; // Generate a number between 1 and 10
-    }
-
-    public int getNumber() {
-        return number;
     }
 }
